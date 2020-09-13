3b3e87f3ae81f9aab6856870cca74e62d7393c66
Stolyarov <t9818184233@gmailcom>
2020-09-13 14:49:57 +0300

    preparation

--- src/index.js
+++ src/index.js
@@ -9,3 +9,23 @@ import BookstoreService from './services/bookstore-service';
 import { BookstoreServiceProvider } from './components/bookstore-service-context';
 
 import store from './store';
+
+const bookstoreService = new BookstoreService();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ErrorBoundry>
+      <BookstoreServiceProvider value={bookstoreService}>
+        <Router>
+          <App />
+        </Router>
+      </BookstoreServiceProvider>
+    </ErrorBoundry>
+  </Provider>,
+  document.getElementById('root')
+);
+/**
+  Все компоненты App имеют доступ к роутингу, имеют доступ к
+  bookstoreService. Если в них будут ошибки эти ошибки будут отлавливаться
+  ErrorBoundry. Все компоненты имеют доступ к redux-store.
+ */
