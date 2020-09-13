46ced98271b3bf18bab7b7c535df7cf22284d6f8
Stolyarov <t9818184233@gmailcom>
2020-09-13 14:34:25 +0300

    preparation

---
+++ src/components/error-indicator/index.js
@@ -0,0 +1,1 @@
+import ErrorIndicator from './Error-indicator';
---
+++ src/components/hoc/with-bookstore-service.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BookstoreServiceConsumer } from '../bookstore-service-context';
+
+const withBookstoreService = () => Wrapper => {
+  return props => {
+    return (
+      <BookstoreServiceConsumer>
+        {bookstoreService => {
+          return <Wrapper {...props} bookstoreService={bookstoreService} />;
+        }}
+      </BookstoreServiceConsumer>
+    );
+  };
+};
+
+export default withBookstoreService;
+
+/**
+  withBookstoreService это функция которая возвращает функцию
+  которая принимает оборочиваемый компонент (в HOC создаем новый 
+  компонент). Возвращаем функцию которая принимает props и эта 
+  функция возвращает JSX рахметку (компонент). В теле компоненты
+  используем BookstoreServiceConsumer для того чтобы получить доступ 
+  к сервису. Оборачиваем wrap-компонент в BookstoreServiceConsumer.
+  Для того что бы получить данные из consumer нужно передать 
+  рендер-фунцию. Эта функция которая примет в качестве своего 
+  значения тот сервис который передадим через контекст, и возвращает 
+  тот самый компонент который оборачиваем и передаем в него все свойства
+  который получил собственный компонент и добавляем еще одно свойство
+  bookstoreService, которой получили из контекста. Теперь можно создавать компоненты который будет получать bookstoreService из контекста.
+ */
